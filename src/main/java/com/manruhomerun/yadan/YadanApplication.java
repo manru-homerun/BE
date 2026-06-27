b0d6f7c6c527cd45aafbfd92feea1f3a78660300
yaongmeow <minnie_00@naver.com>
2026-06-27 15:41:12 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/manruhomerun/yadan/YadanApplication.java
@@ -0,0 +1,13 @@
+package com.manruhomerun.yadan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YadanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YadanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manruhomerun/yadan/YadanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manruhomerun.yadan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YadanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
