8f81f7003d3448493153eab5b4ce0208879da5a1
yaongmeow <minnie_00@naver.com>
2026-07-11 16:00:26 +0900

    fix: 야구 데이터 세팅 sql 스크립트에 지역 코드 추가

--- src/main/resources/db/seed/kbo_baseball_seed.sql
+++ src/main/resources/db/seed/kbo_baseball_seed.sql
@@ -1,20 +1,21 @@
 START TRANSACTION;
 
-INSERT INTO baseball_stadium (id, name, latitude, longitude)
+INSERT INTO baseball_stadium (id, name, latitude, longitude, region_code)
 VALUES
-    (1, '광주-KIA 챔피언스 필드', 35.168139, 126.889111),
-    (2, '잠실야구장', 37.512389, 127.071972),
-    (3, '대전 한화생명 볼파크', 36.316250, 127.431444),
-    (4, '수원 KT 위즈 파크', 37.299778, 127.009667),
-    (5, '고척 스카이돔', 37.498222, 126.867250),
-    (6, '대구삼성라이온즈파크', 35.840917, 128.681611),
-    (7, '인천 SSG 랜더스필드', 37.436778, 126.693306),
-    (8, '사직야구장', 35.194028, 129.061611),
-    (9, '창원 NC 파크', 35.222444, 128.581833)
+    (1, '광주-KIA 챔피언스 필드', 35.168139, 126.889111, '12000'),
+    (2, '잠실야구장', 37.512389, 127.071972, '11000'),
+    (3, '대전 한화생명 볼파크', 36.316250, 127.431444, '30000'),
+    (4, '수원 KT 위즈 파크', 37.299778, 127.009667, '41110'),
+    (5, '고척 스카이돔', 37.498222, 126.867250, '11000'),
+    (6, '대구삼성라이온즈파크', 35.840917, 128.681611, '27000'),
+    (7, '인천 SSG 랜더스필드', 37.436778, 126.693306, '28000'),
+    (8, '사직야구장', 35.194028, 129.061611, '26000'),
+    (9, '창원 NC 파크', 35.222444, 128.581833, '48120')
 ON DUPLICATE KEY UPDATE
     name = VALUES(name),
     latitude = VALUES(latitude),
-    longitude = VALUES(longitude);
+    longitude = VALUES(longitude),
+    region_code = VALUES(region_code);
 
 INSERT INTO baseball_team (id, home_stadium_id, team_name, logo_image)
 VALUES
