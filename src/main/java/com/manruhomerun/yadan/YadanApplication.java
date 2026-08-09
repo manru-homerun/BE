0b906f77eaab1b967d2f3d282166c704290c88ef
jjwon1230 <jjwon1230@naver.com>
2026-08-09 15:40:50 +0900

    chore: 카카오 API, JWT 관련 환경변수 설정

--- src/main/java/com/manruhomerun/yadan/YadanApplication.java
+++ src/main/java/com/manruhomerun/yadan/YadanApplication.java
@@ -1,12 +1,18 @@
 package com.manruhomerun.yadan;
 
+import com.manruhomerun.yadan.auth.properties.JwtProperties;
+import com.manruhomerun.yadan.auth.properties.KakaoApiProperties;
 import com.manruhomerun.yadan.global.properties.TourApiProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({TourApiProperties.class})
+@EnableConfigurationProperties({
+		TourApiProperties.class,
+		KakaoApiProperties.class,
+		JwtProperties.class
+})
 public class YadanApplication {
 
 	public static void main(String[] args) {
