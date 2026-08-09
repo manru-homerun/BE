55222d562d7623b6ab40c176b0dabeac3db4a6ff
jjwon1230 <jjwon1230@naver.com>
2026-08-09 15:44:53 +0900

    feat: User 레포지토리

--- src/main/java/com/manruhomerun/yadan/user/repository/UserRepository.java
+++ src/main/java/com/manruhomerun/yadan/user/repository/UserRepository.java
@@ -1,8 +1,16 @@
 package com.manruhomerun.yadan.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.manruhomerun.yadan.user.domain.entity.User;
+import com.manruhomerun.yadan.user.domain.enums.UserProvider;
 
 public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByProviderAndProviderUserId(
+            UserProvider provider,
+            String providerUserId
+    );
 }
