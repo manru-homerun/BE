42e2fc592c6119173aa0400d2c8ebe9eca89df1d
jjwon1230 <jjwon1230@naver.com>
2026-08-13 18:43:11 +0900

    feat: 지역코드 변환 메서드

---
+++ src/main/java/com/manruhomerun/yadan/user/domain/converter/TravelRegionCodeConverter.java
@@ -0,0 +1,19 @@
+package com.manruhomerun.yadan.user.domain.converter;
+
+import com.manruhomerun.yadan.user.domain.enums.TravelRegionCode;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter
+public class TravelRegionCodeConverter implements AttributeConverter<TravelRegionCode, String> {
+
+    @Override
+    public String convertToDatabaseColumn(TravelRegionCode attribute) {
+        return attribute == null ? null : attribute.getCode();
+    }
+
+    @Override
+    public TravelRegionCode convertToEntityAttribute(String dbData) {
+        return dbData == null ? null : TravelRegionCode.fromCode(dbData);
+    }
+}
