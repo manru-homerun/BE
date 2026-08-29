528bbe7bf169f3dcb149c07803de10412f57a568
jjwon1230 <jjwon1230@naver.com>
2026-08-29 16:21:13 +0900

    fix: 사용자 조회 요청 스웨거 수정

--- src/main/java/com/manruhomerun/yadan/user/dto/UserSearchRequest.java
+++ src/main/java/com/manruhomerun/yadan/user/dto/UserSearchRequest.java
@@ -9,7 +9,7 @@ import jakarta.validation.constraints.Size;
 @Schema(description = "사용자 닉네임 검색 요청")
 public record UserSearchRequest(
 
-        @Schema(description = "검색할 닉네임 앞부분", example = "야구")
+        @Schema(description = "검색할 닉네임", example = "야구")
         @NotBlank(message = "검색할 닉네임은 필수입니다.")
         @Size(max = 12, message = "닉네임 검색어는 12자 이하여야 합니다.")
         String nickname,
