94fdba9752c8f71d80fb700c82acdc37647dd6a5
jjwon1230 <jjwon1230@naver.com>
2026-09-05 00:53:33 +0900

    feat: 알림 설정 조회 서비스

---
+++ src/main/java/com/manruhomerun/yadan/notification/service/NotificationSettingService.java
@@ -0,0 +1,32 @@
+package com.manruhomerun.yadan.notification.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.manruhomerun.yadan.global.error.exception.UserNotFoundException;
+import com.manruhomerun.yadan.notification.domain.entity.NotificationSetting;
+import com.manruhomerun.yadan.notification.dto.NotificationSettingResponse;
+import com.manruhomerun.yadan.notification.error.exception.NotificationSettingNotFoundException;
+import com.manruhomerun.yadan.notification.repository.NotificationSettingRepository;
+import com.manruhomerun.yadan.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class NotificationSettingService {
+
+    private final UserRepository userRepository;
+    private final NotificationSettingRepository notificationSettingRepository;
+
+    public NotificationSettingResponse getSettings(String userId) {
+        userRepository.findById(userId)
+                .orElseThrow(UserNotFoundException::new);
+
+        NotificationSetting notificationSetting = notificationSettingRepository.findByUserId(userId)
+                .orElseThrow(NotificationSettingNotFoundException::new);
+
+        return NotificationSettingResponse.from(notificationSetting);
+    }
+}
