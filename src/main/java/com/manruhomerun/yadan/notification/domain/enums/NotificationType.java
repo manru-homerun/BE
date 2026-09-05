25b68554eb5ad5a25fb705a35fe7e53e5b64fcc5
jjwon1230 <jjwon1230@naver.com>
2026-09-05 12:32:00 +0900

    feat: 알림 목록 조회 종류 enum

---
+++ src/main/java/com/manruhomerun/yadan/notification/domain/enums/NotificationType.java
@@ -0,0 +1,9 @@
+package com.manruhomerun.yadan.notification.domain.enums;
+
+public enum NotificationType {
+    FRIEND_REQUEST,
+    FRIEND_REQUEST_ACCEPTED,
+    TICKET_OPEN,
+    VISIT_VERIFICATION_REMINDER,
+    NEARBY_GAME
+}
