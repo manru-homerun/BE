a7bdade40181931a392c246f018bee6db4577c64
jjwon1230 <jjwon1230@naver.com>
2026-09-10 20:22:23 +0900

    feat: 탈퇴시간 추가

--- src/main/java/com/manruhomerun/yadan/user/domain/entity/User.java
+++ src/main/java/com/manruhomerun/yadan/user/domain/entity/User.java
@@ -80,6 +80,9 @@ public class User {
     @Column(name = "is_deleted", nullable = false)
     private Boolean isDeleted;
 
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
@@ -143,5 +146,15 @@ public class User {
         this.favoriteTeam = favoriteTeam;
         this.birthday = birthday;
         this.gender = gender;
+    }
+
+    public void withdraw() {
+        this.isDeleted = true;
+        this.deletedAt = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+        this.nickname = null;
+        this.profileImageUrl = null;
+        this.gender = null;
+        this.birthday = null;
+        this.favoriteTeam = null;
     }
 }
