7b9310135fb13081646e2951f50ac3f30c3e846a
jjwon1230 <jjwon1230@naver.com>
2026-09-20 01:02:22 +0900

    feat: 경기 알림 레포지토리 - 이번주 경기 조회

--- src/main/java/com/manruhomerun/yadan/baseball/repository/BaseballGameRepository.java
+++ src/main/java/com/manruhomerun/yadan/baseball/repository/BaseballGameRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,6 +13,12 @@ import org.springframework.data.repository.query.Param;
 import com.manruhomerun.yadan.baseball.domain.entity.BaseballGame;
 
 public interface BaseballGameRepository extends JpaRepository<BaseballGame, Long> {
+
+    @EntityGraph(attributePaths = {"homeTeam", "awayTeam"})
+    List<BaseballGame> findAllByGameDateGreaterThanEqualAndGameDateLessThanOrderByGameDateAscIdAsc(
+            LocalDateTime startDateTime,
+            LocalDateTime endDateTime
+    );
 
     Page<BaseballGame> findByStadiumIdAndGameDateGreaterThanEqualOrderByGameDateAscIdAsc(
             Long stadiumId,
