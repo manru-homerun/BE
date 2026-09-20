2674b82fd415743d9545b27a44b8c65779eebcce
jjwon1230 <jjwon1230@naver.com>
2026-09-20 23:24:05 +0900

    fix: 알림 타입 정리

--- src/main/java/com/manruhomerun/yadan/notification/domain/enums/NotificationType.java
+++ src/main/java/com/manruhomerun/yadan/notification/domain/enums/NotificationType.java
@@ -3,9 +3,6 @@ package com.manruhomerun.yadan.notification.domain.enums;
 public enum NotificationType {
     FRIEND_REQUEST,
     FRIEND_REQUEST_ACCEPTED,
-    TICKET_OPEN,
     // referenceId에는 응원팀 ID를 저장한다.
-    WEEKLY_TEAM_SCHEDULE,
-    VISIT_VERIFICATION_REMINDER,
-    NEARBY_GAME
+    WEEKLY_TEAM_SCHEDULE
 }
